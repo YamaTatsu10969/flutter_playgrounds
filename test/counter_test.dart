fe2694ad0aed412ea5e7006fa4d5ef59b2e75b52
YamaTatsu10969 <52.wadachi.96@gmail.com>
2021-07-25 22:38:04 +0900

    add group test

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -4,11 +4,25 @@ import 'package:flutter_playground/counter.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Counter value should be incremented', () {
-    final counter = CounterTest();
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(CounterTest().value, 0);
+    });
 
-    counter.increment();
+    test('value should be incremented', () {
+      final counter = CounterTest();
 
-    expect(counter.value, 1);
+      counter.increment();
+
+      expect(counter.value, 1);
+    });
+
+    test('value should be decremented', () {
+      final counter = CounterTest();
+
+      counter.decrement();
+
+      expect(counter.value, -1);
+    });
   });
 }
